73e2474681486fe5dc313f80418ecb206e7d7208
Jakub <soldzik@gmail.com>
2021-09-15 01:45:40 +0100

    Create FizzBuzz.cpp

---
+++ FizzBuzz.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int FizzBuzz(){
+    int num;
+    int i;
+    cout << "Please input number: "; cin >> num;
+
+    // to include input num in range
+    for (i = 1; i <= num; i++){
+        if (i % 3 == 0){
+            cout << i << " Fizz" << endl;
+        }
+        else if (i % 5 == 0){
+            cout << i << " Buzz" << endl;
+        }
+        else if (i % 3 == 0 || i % 5 == 0){
+            cout << i << " FizzBuzz" << endl;
+        }
+        else
+            cout << i << endl;
+        
+    }
+
+    return 0;
+}
+
+int main(){
+    FizzBuzz();
+}
