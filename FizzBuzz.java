3f55194e667567fa4b429af9038452d4c2ec4793
Jakub <81304425+s0lda@users.noreply.github.com>
2021-12-04 22:10:14 +0000

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+    public static void Fizz_Buzz(int number) {
+        for (int i = 1; i <= number; i++) {
+            if (i % 3 == 0 & i % 5 == 0) {
+                System.out.println(i + " FizzBuzz");
+            }
+            if (i % 3 == 0) {
+                System.out.println(i + " Fizz");
+            }
+            if (i % 5 == 0) {
+                System.out.println(i + " Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Please enter the number: ");
+        Scanner numberScan = new Scanner(System.in);
+        int number = numberScan.nextInt();
+        Fizz_Buzz(number);
+        numberScan.close();
+    }
+}
